0a779d8c412f95526ba290892c1c00710fdf1261
Nokukhanya Makhanya <nokhanyamakhanya@gmail.com>
2022-03-31 18:14:04 +0200

    working API

--- index.js
+++ index.js
@@ -1,142 +1,72 @@
-//const { links } = require("express/lib/response");
-
-//console.log(axios)
-const contacts = document.querySelector(".contacts")
+const results = document.querySelector(".results")
 const carColor = document.querySelector(".carColor")
 const carMakes = document.querySelector(".carMakes")
 
+let colorSelected = ''
+let makeSelected = ''
 
-
-// axios
-//     .get("https://api-tutor.herokuapp.com/v1/cars")
-//     .then(function (response) {
-//         //console.log(response.data)
-//         response.data.forEach(user => {
-//             const li = document.createElement('tr')
-//             li.innerHTML = `
-//             </tr>
-//                 <td>${user.make}</td> 
-//                 <td>${user.model}</td>
-//                 <td>${user.color}</td>
-//                 <td>${user.price}</td>
-//                 <td>${user.reg_number}</td>
-//             </tr>`
-//             contacts.appendChild(li);
-//         })
-//     });
-
-// axios
-//     .get("https://api-tutor.herokuapp.com/v1/colors")
-//     .then(function (response) {
-//         //console.log(response.data)
-//         response.data.forEach(user => {
-//             const li = document.createElement('tr')
-//             li.innerHTML = `
-//             </tr>
-//                 <td>${user}</td> 
-//             </tr>`
-//             carColor.appendChild(li);
-
-//         })
-
-//     });
-
-//     axios
-//     .get("https://api-tutor.herokuapp.com/v1/makes")
-//     .then(function (response) {
-//         //console.log(response.data)
-//         response.data.forEach(user => {
-//             const li = document.createElement('tr')
-//             li.innerHTML = `
-//             </tr>
-//                 <td>${user}</td> 
-//             </tr>`
-//             carMakes.appendChild(li);
-
-//         })
-
-//     });
-
-// async function getData() {
-//     const response = await axios.get("https:jsonplaceholder.typicode.com/users")
-//     response.data.forEach(user => {
-//         const li = document.createElement('tr')
-//         li.innerHTML = `
-//                     </tr>
-//                         <td>${user.name}</td>
-//                         <td>${user.username}</td>
-//                         <td>${user.email}</td></td>
-//                     </tr>`
-//         contacts.appendChild(li);
-
-//     })
-// }
-
-// getData();
-
-
-const wrapper = document.querySelector(".wrapper")
-function clickedColor(sentence) {
-    let displayCarColor=sentence.target.innerHTML
-    console.log(displayCarColor)
-    axios
-    .get(`https://api-tutor.herokuapp.com/v1/cars/color/${displayCarColor}`)
-    .then(function (response) {
-        console.log(response.data)
-        response.data.forEach(user => {
-            const li = document.createElement('tr')
-            li.innerHTML = `
-            </tr>
-                <td>${JSON.stringify(user)}</td> 
-            </tr>`
-            carMakes.appendChild(li);
-        })
-    });
-    return displayCarColor
+function getSelectedColor() {
+    return colorSelected = document.getElementById("colorList").value;
+}
+function getSelectedMake() {
+    makeSelected = document.getElementById("listMake").value;
 }
 
-const ul = document.querySelector(".dropdown");
-wrapper.addEventListener('click', clickedColor)
+const btn = document.querySelector(".btn")
 
-const wrappers = document.querySelector(".wrappers")
-function clickedMake(sentence) {
-    let displayCarMake=sentence.target.innerHTML
-    console.log(displayCarMake)
-    axios
-    .get(`https://api-tutor.herokuapp.com/v1/cars/make/${displayCarMake}`)
-    .then(function (response) {
-        console.log(response.data)
-        response.data.forEach(user => {
-            const li = document.createElement('tr')
-            li.innerHTML = `
+btn.addEventListener("click", function () {
+    let carColor = colorSelected
+    let carMake = makeSelected
+    if (carColor.length != 0 && carMake.length != 0) {
+        results.innerHTML='';
+        axios
+            .get(`https://api-tutor.herokuapp.com/v1/cars/make/${carMake}/color/${carColor}`)
+            .then(function (response) {
+                response.data.forEach(user => {
+                    const li = document.createElement('tr')
+                    li.innerHTML = `
             </tr>
                 <td>${JSON.stringify(user)}</td> 
             </tr>`
-            carColor.appendChild(li);
-        })
-    });
-    return displayCarMake
-}
-
-const uls = document.querySelector(".dropdowns");
-wrappers.addEventListener('click', clickedMake)
-
-    // let displayCarColor=displayCarColor
-    // console.log(displayCarColor)
-    // let displayCarMake=displayCarMake
-    // console.log(displayCarMake)
-
-    axios
-    .get(`https://api-tutor.herokuapp.com/v1/cars/make/toyota/color/blue`)
-    .then(function (response) {
-        console.log(response.data)
-        response.data.forEach(user => {
-            const li = document.createElement('tr')
-            li.innerHTML = `
+                    results.appendChild(li);
+                })
+            });
+    } else if (carColor.length != 0 && carMake.length == 0) {
+        results.innerHTML='';
+        axios
+            .get(`https://api-tutor.herokuapp.com/v1/cars/color/${carColor}`)
+            .then(function (response) {
+                response.data.forEach(user => {
+                    const li = document.createElement('tr')
+                    li.innerHTML = `
             </tr>
                 <td>${JSON.stringify(user)}</td> 
             </tr>`
-            contacts.appendChild(li);
-        })
-    });
-    
+                    results.appendChild(li);
+                })
+            });
+            
+    } else if (carColor.length == 0 && carMake.length != 0) {
+        results.innerHTML='';
+        axios
+            .get(`https://api-tutor.herokuapp.com/v1/cars/make/${carMake}`)
+            .then(function (response) {
+                response.data.forEach(user => {
+                    const li = document.createElement('tr')
+                    li.innerHTML = `
+            </tr>
+                <td>${JSON.stringify(user)}</td> 
+            </tr>`
+                    results.appendChild(li);
+                })
+            });
+
+    } else if (carColor.length == 0 && carMake.length == 0) {
+        results.innerHTML='';
+        results.innerHTML='Nothing was selected'
+    }
+
+})
+
+
+
